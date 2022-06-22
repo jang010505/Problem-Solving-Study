0dfd28b830a64accdde9ca434ae6d6ec366f75bd
hg_yellow <jang010505@gmail.com>
2022-06-22 21:12:35 +0900

    feat: upload 72412.py
    프로그래머스 level2 순위 검색

---
+++ programmers/level2/72412.py
@@ -0,0 +1,123 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+def solution(info, query):
+
+    answer = []
+    language_dict = {"cpp":0, "java":1, "python":2, "-":3}
+    job_dict = {"backend":0, "frontend":1, "-":2}
+    career_dict = {"junior":0, "senior":1, "-":2}
+    food_dict = {"chicken":0, "pizza":1, "-":2}
+
+    group = []
+
+    for i in range(4):
+        group.append([])
+        for j in range(3):
+            group[i].append([])
+            for k in range(3):
+                group[i][j].append([])
+                for l in range(3):
+                    group[i][j][k].append([])
+
+    for temp in info:
+        language, job, career, food, grade = temp.split()
+
+        languages = [language_dict[language], 3]
+        jobs = [job_dict[job], 2]
+        careers = [career_dict[career], 2]
+        foods = [food_dict[food], 2]
+        grade = int(grade)
+
+        for language in languages:
+            for job in jobs:
+                for career in careers:
+                    for food in foods:
+                        group[language][job][career][food].append(grade)
+
+    for i in range(4):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    group[i][j][k][l].sort()
+
+    for string in query:
+        language, temp1, job, temp2, career, temp3, food, grade = string.split()
+        language = language_dict[language]
+        job = job_dict[job]
+        career = career_dict[career]
+        food = food_dict[food]
+        grade = int(grade)
+        n = len(group[language][job][career][food])
+        left = 0
+        right = n
+
+        while left < right:
+            mid = (left+right)//2
+            if group[language][job][career][food][mid] >= grade:
+                right = mid
+            else:
+                left = mid+1
+
+        answer.append(n-right)
+
+    return answer
