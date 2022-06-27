c9ccb5dcbb8b4d158f40ecd84f4b070b395bd88e
hg_yellow <jang010505@gmail.com>
2022-06-27 19:01:58 +0900

    feat: upload 42883.py
    프로그래머스 level2 큰 수 만들기

---
+++ programmers/level2/42883.py
@@ -0,0 +1,11 @@
+def solution(number, k):
+    stack = list()
+
+    for num in number:
+        while k > 0 and stack and stack[-1] < num:
+            stack.pop()
+            k -= 1
+
+        stack.append(num)
+
+    return "".join(stack[:len(stack) - k])
