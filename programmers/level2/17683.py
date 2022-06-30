a81386d8447aedd0c3e40a5b29d8ddd6bc527a7a
hg_yellow <jang010505@gmail.com>
2022-07-01 03:04:14 +0900

    feat: upload 17683.py
    프로그래머스 level2 [3차] 방금그곡

--- programmers/level2/17683.py
+++ programmers/level2/17683.py
@@ -1,38 +1,36 @@
+def getTime(start, end):
+    startH, startM = map(int, start.split(":"))
+    endH, endM = map(int, end.split(":"))
+    return (endH * 60 + endM) - (startH * 60 + startM)
+
+
 def solution(m, musicinfos):
+    m = m.replace("C#", "c")
+    m = m.replace("D#", "d")
+    m = m.replace("F#", "f")
+    m = m.replace("G#", "g")
+    m = m.replace("A#", "a")
 
-    def tokenizing(string):
-        chars = {'C#': 'c',
-                 'D#': 'd',
-                 'F#': 'f',
-                 'G#': 'g',
-                 'A#': 'a',
-                 'E#': 'e'}
+    lst = list()
 
-        melody = []
-        for i in range(len(string)-1):
-            if string[i+1] == '#':
-                melody.append(chars[string[i:i+2]])
-            elif string[i] != '#':
-                melody.append(string[i])
-        if string[i+1] != '#':
-            melody.append(string[i+1])
-        return ''.join(melody)
+    for i, musicinfo in enumerate(musicinfos):
+        startTime, endTime, name, melody = musicinfo.split(",")
+        time = getTime(startTime, endTime)
 
-    def convert(t):
-        hour, minute = t.split(':')
-        return (int(hour)*60)+int(minute)
+        melody = melody.replace("C#", "c")
+        melody = melody.replace("D#", "d")
+        melody = melody.replace("F#", "f")
+        melody = melody.replace("G#", "g")
+        melody = melody.replace("A#", "a")
 
-    m = tokenizing(m)
-    song = dict()
-    for info in musicinfos:
-        start, end, title, melody = info.split(',')
-        start, end, melody = convert(start), convert(end), tokenizing(melody)
-        elapsed, n = end-start, len(melody)
-        melody = melody * (elapsed//n) + melody[:elapsed%n]
-        song[title] = melody
+        melody = melody * time
+        melody = melody[:time]
+        if m in melody:
+            lst.append([time, name, i])
 
-    k = v = ''
-    for key, value in song.items():
-        if (m in value) and (len(v) < len(value)):
-            k, v = key, value
-    return '(None)' if len(k) == 0 else k
+    lst = sorted(lst, key=lambda x: (-x[0], x[2]))
+
+    if len(lst):
+        return lst[0][1]
+    else:
+        return "(None)"
