e5ab924fcd3ca5dbd0c80470a6dd4bf6a10fe2c2
hg_yellow <jang010505@gmail.com>
2022-07-15 01:38:39 +0900

    feat: Upload 12938.py
    프로그래머스 level3 최고의 집합

---
+++ programmers/level3/12938.py
@@ -0,0 +1,14 @@
+def solution(n, s):
+    answer = []
+    base = s // n
+    rem = s % n
+
+    if base == 0:
+        return [-1]
+
+    for _ in range(n - rem):
+        answer.append(base)
+    for _ in range(rem):
+        answer.append(base + 1)
+
+    return answer
