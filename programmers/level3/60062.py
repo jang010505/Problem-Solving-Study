54d12e2ccd52fbbab4b638e748d222615590a701
hg_yellow <jang010505@gmail.com>
2022-07-20 01:10:31 +0900

    feat: Upload 60062.py
    프로그래머스 level3 외벽 점검

---
+++ programmers/level3/60062.py
@@ -0,0 +1,32 @@
+from itertools import permutations
+
+def solution(n, weak, dist):
+    answer = 0
+    dist = sorted(dist, reverse=True)
+    repair_lst = [()]
+
+    for move in dist:
+        repairs = []
+        answer += 1
+
+        for i, weakPoint in enumerate(weak):
+            start = weakPoint
+            ends = weak[i:] + [n + w for w in weak[:i]]
+            can = set()
+
+            for end in ends:
+                if end-start <= move:
+                    can.add(end%n)
+
+            repairs.append(can)
+        can = set()
+
+        for r in repairs:
+            for x in repair_lst:
+                new = r | set(x)
+                if len(new) == len(weak):
+                    return answer
+                can.add(tuple(new))
+        repair_lst = can
+
+    return -1
